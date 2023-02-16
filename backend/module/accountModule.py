4c5d0449ae34740f9da95c5be60129dbefe15f82
JIN NINGXIAN <jinningxian@hotmail.com>
2023-02-16 19:13:58 +0800

    update deposit and account Module

--- backend/module/accountModule.py
+++ backend/module/accountModule.py
@@ -1,19 +1,57 @@
 from databaseConnection import create_engine
+import time
 
-
+#LOGIN
 def verify_password(username, password):
-  #search for the username then return password from database
-  #password_from_database => get the password from database
   engine = create_engine()
   sql = "SELECT username from user WHERE username='"+username+"' AND password ='"+password+"'"
-  result = engine.execute("SELECT * FROM user_details WHERE userID='None'")
-  if result.rowcount > 0:
+  result = engine.execute(sql).fetchone()
+  if result:
+    userID = result[0]
+    ts = time.time()
+    ts = str(int(ts))
+
+    #update the last log in time 
+    updateSQL = "UPDATE user SET lastLoginTimeStamp='"+str(int(ts))+"' WHERE userID = " + str(userID)
+    engine.execute(updateSQL)
     return {
       "code": 200,
-      "username": username
+      "username": username,
+      "userID": userID
     }, 200
 
   return  {
       "code": 404,
       "username": None
-    }, 404 
+    }
+
+#PEEK
+def get_net_worth(userID):
+  engine = create_engine()
+  sql = "SELECT * FROM deposit_account WHERE userID = "+str(userID)
+  result = engine.execute(sql)
+  if result.rowcount > 0:
+    availBalance = 0.0
+    for info in result.fetchall():
+      availBalance += info[6]
+    return{
+      "code": 200,
+      "AvailBalance": availBalance
+    }
+
+  return {
+    "code": 404,
+    "AvailBalance": 0
+  }
+
+#REGISTER
+def register(userID, username, password):
+  engine = create_engine()
+  userID = engine.execute("SELECT COUNT(*) FROM user").fetchone()[0]
+  sql = "INSERT INTO user (userID, username, password, lastLoginTimeStamp) VALUES ('"+str(userID)+"', '"+str(username)+"', '"+password+"', 'None')"
+  result = engine.execute(sql)
+  engine.execute(result)
+  return {
+    "code": 200,
+    "status": "Register Successful"
+  }
--- backend/module/depositModule.py
+++ backend/module/depositModule.py
@@ -1,13 +1,51 @@
-def get_view_all_deposit_accounts(username):
-    all_deposit_account = {
-        "account1": "XYZ1",
-        "account2": "XYZ2",
-        "account3": "XYZ3",
+from databaseConnection import create_engine
+from classes.deposit_account import Deposit_Account
+def get_view_all_deposit_accounts(userID):
+    engine = create_engine()
+    sql = "SELECT * FROM deposit_account WHERE userID = "+str(userID)
+    result = engine.execute(sql)
+    if result.rowcount>0:
+        accountInfo = []
+        for info in result.fetchall():
+            depositAccountInfo = Deposit_Account(
+                info[0], info[1], info[2], info[3], info[4], info[5], info[6], info[7],
+                info[8], info[9], info[10], info[11], info[12], info[13], info[14], info[15],
+                info[16]
+            ).to_dict()
+            accountInfo.append(depositAccountInfo)
+        return{
+            "code": 200,
+            "accountInfo": accountInfo
+        }
+
+    return {
+        "code": 404,
+        "accountInfo":[
+
+        ]
     }
 
-    return all_deposit_account
 
-def get_transaction_history(username):
-    return None
+#SELECTED
+def get_selected_deposit_account(DepositAccountID):
+    engine = create_engine()
+    sql = "SELECT * FROM deposit_account WHERE DepositAccountID = "+str(DepositAccountID)
+    info = engine.execute(sql).fetchone()
+    if info:
+        depositAccountInfo = Deposit_Account(
+                info[0], info[1], info[2], info[3], info[4], info[5], info[6], info[7],
+                info[8], info[9], info[10], info[11], info[12], info[13], info[14], info[15],
+                info[16]
+            ).to_dict()
+        return{
+            "code": 200,
+            "accountInfo": depositAccountInfo
+        }
+    return {
+        "code": 404,
+        "accountInfo": None
+    }
+
+
 
 
