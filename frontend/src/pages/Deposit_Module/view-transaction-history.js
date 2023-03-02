7fdf08313a82300690925bef26c16f04d041faa8
evelynpehty <evelyn.peh.2020@scis.smu.edu.sg>
2023-03-02 14:01:51 +0800

    visualization

--- frontend/src/pages/Deposit_Module/view-transaction-history.js
+++ frontend/src/pages/Deposit_Module/view-transaction-history.js
@@ -63,53 +63,86 @@ export default function ColorTabs() {
     const {state} = useLocation();
     const { transaction_item, id } = state;
 
-    const groupByYearMonth = transaction_item.reduce((acc, cur) => {
-        const createdDate = new Date(cur.transactionDate)
-        const year = createdDate.getFullYear() + " "
-        const monthShortName = createdDate.toLocaleString('en-us', { month: 'long' })
-      
-        // Get year object corresponding to current item from acc (or insert if not present)
-        const groupByYear = acc[year] = acc[year] || {};
-      
-        //Get month array corresponding to current item from groupByYear object (or insert if not present)
-        const groupByMonth = groupByYear[monthShortName] = groupByYear[monthShortName] || [];
-      
-        // Add current item to current groupByMonth array
-        groupByMonth.push(cur);
-      
-        return acc
-      }, {});
-      
+    const [value, setValue] = React.useState('Monthly');
+    // const [startDate, setStartDate] = React.useState(null);
+    // const [endDate, setEndDate] = React.useState(null);
+    
+
+    function func_groupByYearMonth(transaction_item) {
+        const result = transaction_item.reduce((acc, cur) => {
+            const createdDate = new Date(cur.transactionDate)
+            const year = createdDate.getFullYear() + " "
+            const monthShortName = createdDate.toLocaleString('en-us', { month: 'long' })
+          
+            // Get year object corresponding to current item from acc (or insert if not present)
+            const groupByYear = acc[year] = acc[year] || {};
+          
+            //Get month array corresponding to current item from groupByYear object (or insert if not present)
+            const groupByMonth = groupByYear[monthShortName] = groupByYear[monthShortName] || [];
+          
+            // Add current item to current groupByMonth array
+            groupByMonth.push(cur);
+          
+            return acc
+          }, {});
+
+        return result
+      }
+      var groupByYearMonth = func_groupByYearMonth(transaction_item)
       console.log(groupByYearMonth);
 
-      const groupByYMWeek = transaction_item.reduce((acc, cur) => {
-        const createdDate = new Date(cur.transactionDate)
-        const year = createdDate.getFullYear() + " "
-        const monthShortName = createdDate.toLocaleString('en-us', { month: 'long' })
-        const weekNum = moment(createdDate).format('W')
-        
-        // Get year object corresponding to current item from acc (or insert if not present)
-        const groupByYear = acc[year] = acc[year] || {};
-      
-        //Get month array corresponding to current item from groupByYear object (or insert if not present)
-        const groupByMonth = groupByYear[monthShortName] = groupByYear[monthShortName] || [];
-      
-        //Get week array corresponding to current item from groupByMonth object (or insert if not present)
-        const groupByWeek = groupByMonth[weekNum] = groupByMonth[weekNum] || [];
-        groupByWeek.push(cur);
-       
-        return acc
-      }, {});
-      
+      function func_groupByYMWeek(transaction_item) {
+        const result = transaction_item.reduce((acc, cur) => {
+            const createdDate = new Date(cur.transactionDate)
+            const year = createdDate.getFullYear() + " "
+            const monthShortName = createdDate.toLocaleString('en-us', { month: 'long' })
+            const weekNum = moment(createdDate).format('W')
+            
+            // Get year object corresponding to current item from acc (or insert if not present)
+            const groupByYear = acc[year] = acc[year] || {};
+          
+            //Get month array corresponding to current item from groupByYear object (or insert if not present)
+            const groupByMonth = groupByYear[monthShortName] = groupByYear[monthShortName] || [];
+          
+            //Get week array corresponding to current item from groupByMonth object (or insert if not present)
+            const groupByWeek = groupByMonth[weekNum] = groupByMonth[weekNum] || [];
+            groupByWeek.push(cur);
+           
+            return acc
+          }, {});
+          return result
+      }
+
+    var groupByYMWeek = func_groupByYMWeek(transaction_item)
     console.log(groupByYMWeek);
       
-    const [value, setValue] = React.useState('Monthly');
-
     const handleChange = (event, newValue) => {
     setValue(newValue);
-    console.log(newValue)
     };
 
+    /*const handleFilter = () =>{
+
+        var filtered = transaction_item.filter(function (el)
+        {
+            const transactionDate = moment(el.transactionDate).format("DD-MM-YYYY")
+            const newTransactionDate = moment(transactionDate, "DD-MM-YYYY")
+            const sd = moment(startDate).format("DD-MM-YYYY")
+            const new_sd = moment(sd, "DD-MM-YYYY")
+            const ed = moment(ed).format("DD-MM-YYYY")
+            const new_ed = moment(ed,"DD-MM-YYYY")
+            
+            return newTransactionDate.isBetween(new_sd, new_ed,undefined,[]) 
+        })
+
+        if(value === "Monthly"){
+            groupByYearMonth = func_groupByYearMonth(filtered)
+        }
+        if(value === "Weekly"){
+            groupByYMWeek = func_groupByYMWeek(filtered)
+        }
+        
+        console.log(filtered)
+    }*/
 
   return (
     <Container maxWidth="lg">
@@ -126,6 +159,8 @@ export default function ColorTabs() {
             <Tab value="Weekly" label="Weekly" />
             
         </Tabs>
+        
+       
         <Card style={ styles.card2 } elevation={4}>
         { value === "Monthly" &&
            Object.keys(groupByYearMonth).map(year => {
